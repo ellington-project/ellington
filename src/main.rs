5d1035d5756fcedd91b276bcd463011fa8f26d52
Adam Harries <harries.adam@gmail.com>
2018-06-16 16:14:06 +0100

    start on symbolic library representation

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,61 @@
-extern crate xml; 
 extern crate plist; 
 
 use plist::Plist; 
 
 use std::fs::File; 
-use std::io::BufReader;
-
-use xml::reader::{EventReader, XmlEvent}; 
-use xml::reader::XmlEvent::CData;
-use xml::reader::XmlEvent::Comment;
-use xml::reader::XmlEvent::ProcessingInstruction;
-use xml::reader::XmlEvent::Whitespace;
-
 
 struct Track {
+    iTunesID: u32,
     bpm: u32,
     name: String, 
     location: String,  
 }
 
+fn extract_track(pldict: plist::Plist) -> Option<Track> {
 
-
-fn print_library(filename: &str) -> () {
-    let file = File::open(filename).unwrap(); 
-    let file = BufReader::new(file); 
-
-    let parser = EventReader::new(file);
-    // let mut books = Vec::new();
-    let mut _current_element = String::new();
-
-    let mut indent = "".to_string();
-    for e in parser {
-        match e {
-            Ok(XmlEvent::StartDocument { ..}) => {
-                println!("{}StartDocument", indent);
-                indent.push('\t');
-            },
-            Ok(XmlEvent::EndDocument {.. }) => {
-                indent.pop();
-                println!("{}EndDocument", indent);
-            },
-            Ok(XmlEvent::StartElement { name, .. }) => {
-                println!("{}StartElement: {}", indent, name);
-                indent.push('\t');
-            },
-            Ok(XmlEvent::EndElement { name,  .. }) => {
-                indent.pop();
-                println!("{}EndElement: {}", indent, name);
-            },
-            Ok(CData(_)) => {
-
-            },
-            Ok(Comment(_)) => {
-
-            }, 
-            Ok(ProcessingInstruction { .. }) => {
-
-            },
-            Ok(Whitespace(_)) => {
-
-            },
-            Ok(XmlEvent::Characters(s)) => {
-                println!("{}Characters: {}", indent, s);
-            },
-            Err(e) => {
-                println!("{}Error: {}", indent, e)
-            }
-            // _ => {
-            //     println!("Failed to parse: {}", e);
-            // }
+    return match pldict.as_dictionary(){
+        Some(dict) => {
+            Some(Track {
+                iTunesID: 0, 
+                bpm: 0, 
+                name: "name".to_string(), 
+                location: "here".to_string()
+            })
         }
+        None => None
     }
-
 }
+
 
 fn read_plist(filename: &str) -> () {
     let file = File::open(filename).unwrap();
 
     let plist = Plist::read(file).unwrap(); 
 
-    match plist {
-        Plist::Array(_array) => println!("Array"), 
-        Plist::Dictionary(_) => println!("Dictionary"), 
-        Plist::Boolean(_) => println!("Boolean"), 
-        Plist::Data(_) => println!("Data"),
-        Plist::Date(_) => println!("Date"),
-        Plist::Real(_) => println!("Real"), 
-        Plist::Integer(_) => println!("Integer"), 
-        Plist::String(_) => println!("String")
-        // _ => ()
+    // println!("Overall Plist: {:?}", &plist);
+
+    // get the tracks from the PList: 
+
+    let tracks = plist.as_dictionary().unwrap().get("Tracks").unwrap();
+
+    println!("Found {} tracks in the tracklist", tracks.as_dictionary().unwrap().len());
+
+    // println!("Tracks: {:?}", tracks);
+
+    for (id, track) in tracks.as_dictionary().unwrap().iter() {
+        println!("Found track, id: {}:", id);
+        // get the dictionary for the track: 
+        let dict = track.as_dictionary().unwrap();
+        println!("Name: {:?}", dict.get("Name").unwrap());
+        println!("BPM: {:?}", dict.get("BPM").unwrap());
+        // println!("\t{:?}", track);
+        println!("---");
+
     }
 }
 
 fn main() {
-    // print_library("res/partialLibrary.xml");
     read_plist("res/partialLibrary.xml");
-    println!("Hello, world!");
+    // read_plist("/Users/adam/Music/iTunes/iTunes Music Library.xml");
+    // println!("Hello, world!");
 }
